98a0958be188075f8958b8cb32531239007f11b1
Mariana-Perez <Mariana.Perez@ibm.com>
2019-07-22 12:44:47 -0400

    add semicolon to mysql command

--- test/travis.sql
+++ test/travis.sql
@@ -1,1 +1,4 @@
-CREATE DATABASE microservice;
+USE mysql;
+UPDATE USER set authentication_string=PASSWORD("pass123") WHERE USER="root";
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS microservice;
