8c0cbf71efa9bdcc94fc27289289bc9d22ce98de
Mariana-Perez <Mariana.Perez@ibm.com>
2019-07-22 14:15:06 -0400

    chnage localhost to 127.0.0.1

--- imagini.js
+++ imagini.js
@@ -10,7 +10,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
-  password: '',
+  password: 'pass123',
   database: 'microservice'
 });
 app.db = db;
